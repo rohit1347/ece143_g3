b02e0b56f8bb9d4cfe39851f68ac48360464a59d
Rohit Kumar <rokumar@ucsd.edu>
2019-05-14 18:20:48 -0700

    Updated import_xls

--- Rohit/ProjectWorkspace.py
+++ Rohit/ProjectWorkspace.py
@@ -1,3 +1,5 @@
 cities = {'CA': ['San Diego', 'San Francisco', 'Los Angeles'], 'WA': ['Seattle'], 'TX': ['Austin', 'Houston', 'Dallas'], 'NY': ['New York'], 'IN': ['Chicago'], 'MI': ['Detroit'],
           'GA': ['Atlanta'], 'FL': ['Miami', 'Orlando'], 'AZ': ['Phoenix'], 'PA': ['Philadelphia', 'Pittsburgh'], 'WI': ['Milwaukee', 'Madison'], 'CO': ['Denver'], 'NV': ['Las Vegas'], 'UT': ['Salt Lake City']}
 col_index = [1, 4, 6, 8, 10, 12, 14]
+col_index_names = ['Urban Population', 'Vehicles in Service', 'Vehicles Available',
+                   'Vehicle Revenue Miles', 'Vehicle Revenue Hours', 'Unlinked Passenger Trips', 'Passenger Miles']
--- Rohit/import_xls.py
+++ Rohit/import_xls.py
@@ -11,11 +11,13 @@ from ProjectWorkspace import *
 plt.style.use('classic')
 
 # %%
-# Extracting list of the names of all required excels
 
 
-def get_xls():
-    """Returns a list of excel files in the directory.
+def get_xls(pflag=0):
+    """Returns a list of Excel files found and for what years.
+
+    Keyword Arguments:
+        pflag {int} -- Print flag (default: {0})
     """
     items = os.listdir(".")  # all items in current directory
     datasets = []
@@ -23,41 +25,37 @@ def get_xls():
         if ("Appendix_B.xls" in names) or ("Appendix-B.xls" in names):
             datasets.append(names)
     datasets = sorted(datasets)
-    print(f'Excel files found= {datasets}')
-    return datasets
+    years = []
+    for year in datasets:
+        years.append(''.join([y for y in list(year) if y.isdigit()]))
+    if pflag:
+        print(f'Excel files found= {datasets}\n')
+        print(f'Years= {years}\n')
+    return datasets, years
 
 
 def create_empty_city_dataframes(cities=cities):
-    """Returns a list of empty dataframes for each city in the input dictionary.
+    """Returns a dictionary containing dataframes corresponding to cities.
 
     Keyword Arguments:
-        cities {dict} -- Dict with keys='states', and values='cities' (default: {cities})
+        cities {dict} -- To be imported from ProjectWorksapce (default: {cities})
     """
     assert isinstance(cities, dict)
-    assert all(isinstance(cities[state]) for state in cities.keys())
-    total_cities = 0
+    assert all(isinstance(cities[state], list) for state in cities.keys())
+    # total_cities = 0
+    # for state in cities.keys():
+    #     total_cities += len(cities[state])
+    # dataframe_list = [None] * total_cities
+    dataframe_dict = dict()
     for state in cities.keys():
-        total_cities += len(cities[state])
-    dataframe_list = [None] * total_cities
-    for idx in range(total_cities):
-        dataframe_list[idx] = pd.DataFrame()
+        dataframe_dict[state] = [None]*len(cities[state])
+        for cix, city in enumerate(cities[state]):
+            dataframe_dict[state][cix] = pd.DataFrame(
+                columns=col_index_names, index=get_xls()[-1])
+    # for idx in range(total_cities):
+    #     dataframe_list[idx] = pd.DataFrame(columns=col_index_names)
     print('Created dataframes list')
-    return dataframe_list
-
-
-def get_city_indices(cities=cities, sheet=sheet):
-    assert isinstance(cities, dict)
-    assert all(isinstance(cities[state]) for state in cities.keys())
-    assert sheet.__name__ == 'pandas.core.frame.DataFrame'
-    cindexer = {}  # City indexer
-    cnames = list(sheet.columns)
-    for state in cities.keys():
-        cindexer[state] = list()
-        for city in cities[state]:
-            truevals = sheet[cnames[0]].str.contains(city)
-            temp_index = [i for i, x in enumerate(truevals) if x][-1]
-            cindexer[state].append(temp_index)
-    return(cindexer)
+    return dataframe_dict
 
 
 # %%
@@ -65,9 +63,9 @@ def create_city_dataframes(pflag=0, cities=cities):
     assert isinstance(pflag, int)
     assert pflag == 0 or pflag == 1
     assert isinstance(cities, dict)
-    assert all(isinstance(cities[state]) for state in cities.keys())
+    assert all(isinstance(cities[state], list) for state in cities.keys())
 
-    datasets = get_xls()
+    datasets, years = get_xls()
     filled_frames = create_empty_city_dataframes()
     for excel in datasets:
         sheet = pd.read_excel(excel, sheet_name='UZA Totals', index_col=3)
@@ -75,8 +73,8 @@ def create_city_dataframes(pflag=0, cities=cities):
         if pflag:
             # Prints the columns found in each excel file
             print(f'Columns in {excel}: {cnames}\n')
+        cindexer = get_city_indices()
         for citynum, cframe in enumerate(filled_frames):
-            cindexer = get_city_indices()
             # What is left:
             # (i) You are in a sheet
             # (ii) you have the row indices for the cities in the sheet
@@ -101,8 +99,33 @@ for excel in datasets:
     dataset_index[excel] = cindexer
 print(dataset_index)
 
+# %%
+
+
+def get_city_indices(cities=cities, sheet=sheet):
+    """Returns indices for cities in a pandas dataframe. Function must be called inside `create_city_dataframes()` for now.
+
+    Keyword Arguments:
+        cities {dict} -- To be imported from ProjectWorkspace (default: {cities})
+        sheet {pandas dataframe} -- Dataframe in which indices are to be found. (default: {sheet})
+    """
+    assert isinstance(cities, dict)
+    assert all(isinstance(cities[state], list) for state in cities.keys())
+    assert sheet.__name__ == 'pandas.core.frame.DataFrame'
+    cindexer = {}  # City indexer
+    cnames = list(sheet.columns)
+    for state in cities.keys():
+        cindexer[state] = list()
+        for city in cities[state]:
+            truevals = sheet[cnames[0]].str.contains(city)
+            temp_index = [i for i, x in enumerate(truevals) if x][-1]
+            cindexer[state].append(temp_index)
+    return(cindexer)
+
 
 # %%
 plt.style.use('seaborn-whitegrid')
 apta2007.plot(
     kind='line', x=apta2007.iloc[0][0], y=apta2007.iloc[0][1])
+# %% Testing
+create_empty_city_dataframes()
