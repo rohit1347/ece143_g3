5d67587bbb6cc3c3867216a58b6ea85db6152e6e
Rohit Kumar <rokumar@ucsd.edu>
2019-05-28 23:26:58 -0700

    Update bokeh_choropleth

--- Rajat/read_pdf_HK_env.py
+++ Rajat/read_pdf_HK_env.py
@@ -1,50 +1,56 @@
-#%%
+# %%
 import camelot
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 %matplotlib qt5
 
-#%%
-tables = camelot.read_pdf('123.pdf', pages = '17')     
+# %%
+tables = camelot.read_pdf('123.pdf', pages='17')
 tables.export('abc.csv', f='csv', compress=False)
 
 
-#%%
+# %%
 table = pd.read_csv('abc-page-17-table-1.csv')
 # print(table)
 table = table.iloc[0]
 # a = list(table.columns[1:11].values)
-# b = 
-df = pd.DataFrame({'Years':table.index[1:], 'Electricity consumption (in kWh)':table.values[1:]})
+# b =
+df = pd.DataFrame(
+    {'Years': table.index[1:], 'Electricity consumption (in kWh)': table.values[1:]})
 
-df['Electricity consumption (in kWh)'] = df['Electricity consumption (in kWh)'].str.replace(',', '')
-df['Electricity consumption (in kWh)'] = df['Electricity consumption (in kWh)'].astype(int)
-#%%
-## Total Ridership for HongKong (2013-2017)
+df['Electricity consumption (in kWh)'] = df['Electricity consumption (in kWh)'].str.replace(
+    ',', '')
+df['Electricity consumption (in kWh)'] = df['Electricity consumption (in kWh)'].astype(
+    int)
+# %%
+# Total Ridership for HongKong (2013-2017)
 file = 'table21.xls'
-data = pd.read_excel(file, sheet_name = 0, skiprows = 5)
-data = data.iloc[4:10] ## For years 2013-2018
-data = data.rename(columns = {'Unnamed: 10':'Ridership1','小計\nSub-total':'Ridership2'})
+data = pd.read_excel(file, sheet_name=0, skiprows=5)
+data = data.iloc[4:10]  # For years 2013-2018
+data = data.rename(
+    columns={'Unnamed: 10': 'Ridership1', '小計\nSub-total': 'Ridership2'})
 
-#%%
-##Total ridership from 2007-2017
+# %%
+# Total ridership from 2007-2017
 
 file = 'table11.xls'
-df_2= pd.read_excel(file, skiprows = 8)
+df_2 = pd.read_excel(file, skiprows=8)
 df_2 = df_2.iloc[5:15]  # For years 2007- 2017
-df_2 = df_2.rename(columns = {'Unnamed: 2':'Years','交通乘客人次 (1) (3) (4) ':'Avg. daily ridership',
-'Casualties Involved in Road Accidents (1)':'Killed','Unnamed: 20':'Injured', 'Unnamed: 22':'Killed1','Unnamed: 23':'Injured1'})
+df_2 = df_2.rename(columns={'Unnamed: 2': 'Years', '交通乘客人次 (1) (3) (4) ': 'Avg. daily ridership',
+                            'Casualties Involved in Road Accidents (1)': 'Killed', 'Unnamed: 20': 'Injured', 'Unnamed: 22': 'Killed1', 'Unnamed: 23': 'Injured1'})
 
 
-#%%
+# %%
 df_new2 = pd.DataFrame({'Electricity consumption (in kWh)': list(df['Electricity consumption (in kWh)'].values),
                         'Annual Ridership (in thousands)': list(((df_2['Avg. daily ridership'].values*365/1000)))
-                    }, index = None)  ## HK population is taken as 7.5 million
+                        }, index=None)  # HK population is taken as 7.5 million
 
 df_new2 = df_new2.set_index(df['Years'].values)
 
-#%%
+# %%
+
+
 def create_correlation_plot(df):
     """Create seaborn correlation plot for input data frame.
 
