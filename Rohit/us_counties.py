37cf1bd23c42404edd5dbca86106bfac6639e30d
Rohit Kumar <rokumar@ucsd.edu>
2019-06-06 12:48:36 -0700

    Cleaning dirs

--- Rohit/us_counties.py
+++
@@ -1,5 +0,0 @@
-# %%
-from bokeh.plotting import figure, show, output_file
-from bokeh.sampledata.us_counties import data as counties
-from bokeh.sampledata.us_states import data as states
-from ProjectWorkspace import cities
